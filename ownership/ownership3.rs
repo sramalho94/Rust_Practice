5ab32e65e7c70b6c1e419beffd2f65e840cd965c
Stephan D Ramalho <sramalho@fordham.edu>
2023-08-23 11:39:12 -0400

    more practice with ownership

---
+++ ownership/ownership3.rs
@@ -0,0 +1,12 @@
+fn main(){
+  let s = give_ownership();
+  println!("{}", s);
+}
+
+// Only modify the code below!
+fn give_ownership() -> String {
+  let s: String = String::from("hello, world");
+  // Convert String to Vec
+  let _s = s.as_bytes();
+  s
+}
