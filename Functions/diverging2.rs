d07f946f980de9d7d03692859505b6de2ca7504c
Stephan D Ramalho <sramalho@fordham.edu>
2023-08-23 10:50:33 -0400

    practice using panic in diverging functions

---
+++ Functions/diverging2.rs
@@ -0,0 +1,16 @@
+fn main(){
+  // FILL in the blank
+
+  let b = false;
+
+  let _v = match b {
+    true => 1,
+    // Diverging functions can also be used in match expression to replace a val
+    false => {
+      println!("Success!");
+      panic!("We have no value for `false` but we can panic");
+    }
+  };
+
+  println!("Exercise Failed if printing out this line!");
+}
