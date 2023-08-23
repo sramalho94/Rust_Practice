68a35fe377dde3329d0e7310363343265f1a2506
Stephan D Ramalho <sramalho@fordham.edu>
2023-08-23 11:43:33 -0400

    practice cloning strings to preserve ownership

---
+++ ownership/ownership4.rs
@@ -0,0 +1,13 @@
+// Fix the error without removing the code line
+
+fn main(){
+  let s = String::from("hello, world");
+
+  print_str(s.clone());
+
+  println!("{}", s);
+}
+
+fn print_str(s: String) {
+  println!("{}", s);
+}
