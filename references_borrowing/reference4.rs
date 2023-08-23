2248115f457572c97755a9a3e1bfd33fc43324f0
Stephan D Ramalho <sramalho@fordham.edu>
2023-08-23 16:28:55 -0400

    practice with mutable reference to strings

---
+++ references_borrowing/reference4.rs
@@ -0,0 +1,13 @@
+// Fix the error
+
+fn main(){
+  let mut s: String = String::from("hello, ");
+
+  push_str(&mut s);
+
+  println!("Success!");
+}
+
+fn push_str(s: &mut String) {
+  s.push_str("world")
+}
