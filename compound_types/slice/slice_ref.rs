8ecc86724b5f4319dd2b0ead0a720b6f603e0098
Stephan D Ramalho <sramalho@fordham.edu>
2023-08-28 10:41:59 -0400

    refactored ln 9 slice references hold 16 bytes

--- compound_types/slice/slice_ref.rs
+++ compound_types/slice/slice_ref.rs
@@ -6,7 +6,7 @@ fn main() {
   
   // Modify '8' to make it work
   // TIPS: slice( reference ) IS NOT an array, if it is an array, then `assert!` will be passed: Each of the two chars '中' and '国'  occupies 4 bytes, 2 * 4 = 8
-  assert!(std::mem::size_of_val(slice) == 8);
+  assert!(std::mem::size_of_val(&slice) == 16);
 
   println!("Success!");
 }
