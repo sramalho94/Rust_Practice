6a7f3dd790e535a66236c29be08ffbefb0d00c3b
Stephan D Ramalho <sramalho@fordham.edu>
2023-08-29 16:06:47 -0400

    practice with option

--- compound_types/enum/enum5.rs
+++ compound_types/enum/enum5.rs
@@ -2,22 +2,24 @@
 // Fill in the blank to make the `println` work.
 // Also add some code to prevent the `panic` from running.
 fn main() {
-  let five = Some(5);
-  let six = plus_one(five);
+  let five: Option<i32> = Some(5);
+  let six: Option<i32> = plus_one(five);
   let none = plus_one(None);
 
-  if let __ = six {
+  if let Some(n) = six {
       println!("{}", n);
 
       println!("Success!");
-  } 
+  }  else {
+    panic!("NEVER LET THIS RUN！");
+  }
       
-  panic!("NEVER LET THIS RUN！");
+  
 } 
 
 fn plus_one(x: Option<i32>) -> Option<i32> {
   match x {
-      __ => None,
-      __ => Some(i + 1),
+      None => None,
+      Some(i) => Some(i + 1),
   }
 }
