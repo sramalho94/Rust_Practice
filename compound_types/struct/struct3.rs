a895ff89601ef0fba092eee89789d309b3d93d76
Stephan D Ramalho <sramalho@fordham.edu>
2023-08-29 11:07:41 -0400

    created files for practice

---
+++ compound_types/struct/struct3.rs
@@ -0,0 +1,17 @@
+
+// Fix the error and fill the blanks
+struct Color(i32, i32, i32);
+struct Point(i32, i32, i32);
+fn main() {
+    let v = Point(__, __, __);
+    check_color(v);
+
+    println!("Success!");
+}   
+
+fn check_color(p: Color) {
+    let (x, _, _) = p;
+    assert_eq!(x, 0);
+    assert_eq!(p.1, 127);
+    assert_eq!(__, 255);
+ }
