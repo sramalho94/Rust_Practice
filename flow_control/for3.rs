ea970bc13ed78f1fe2affc1355a5e6dd48ee736b
Stephan D Ramalho <sramalho@fordham.edu>
2023-08-30 09:40:36 -0400

    practice with iterating and enumerating for loops

--- flow_control/for3.rs
+++ flow_control/for3.rs
@@ -2,7 +2,7 @@ fn main() {
   let a = [4, 3, 2, 1];
 
   // Iterate the indexing and value in 'a'
-  for (i,v) in a.__ {
+  for (i,v) in a.iter().enumerate() {
       println!("The {}th element is {}",i+1,v);
   }
 }
