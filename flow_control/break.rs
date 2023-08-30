d8b9214307f1d83f4ddf4d5c2146e64b00b57657
Stephan D Ramalho <sramalho@fordham.edu>
2023-08-30 10:04:56 -0400

    practice breaking out of for loops

--- flow_control/break.rs
+++ flow_control/break.rs
@@ -3,10 +3,10 @@
 fn main() {
   let mut n = 0;
   for i in 0..=100 {
-     if n == 66 {
-         __
-     }
-     n += 1;
+    if n == 66 {
+        break
+    }
+    n += 1;
   }
 
   assert_eq!(n, 66);
