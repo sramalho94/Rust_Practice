032eb7c7ee58eb50c67347f68931cebe66c291e9
Stephan D Ramalho <sramalho@fordham.edu>
2023-08-30 09:33:50 -0400

    practice with for loops, modified ln 5 to exclude 100

--- flow_control/for.rs
+++ flow_control/for.rs
@@ -2,7 +2,7 @@
 
 
 fn main() {
-  for n in 1..=100 { // modify this line to make the code work
+  for n in 1..100 { // modify this line to make the code work
       if n == 100 {
           panic!("NEVER LET THIS RUN")
       }
