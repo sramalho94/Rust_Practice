03bd7022f387006f17e1087dc5219d9251e1000b
Stephan D Ramalho <sramalho@fordham.edu>
2023-09-01 11:48:53 -0400

    practice using match guards

--- pattern_match/patterns/patterns4.rs
+++ pattern_match/patterns/patterns4.rs
@@ -2,10 +2,10 @@
 
 // Fill in the blank to make the code work, `split` MUST be used
 fn main() {
-  let num = Some(4);
-  let split = 5;
+  let num: Option<i32> = Some(4);
+  let split: i32 = 5;
   match num {
-      Some(x) __ => assert!(x < split),
+      Some(x) if x < split => assert!(x < split),
       Some(x) => assert!(x >= split),
       None => (),
   }
