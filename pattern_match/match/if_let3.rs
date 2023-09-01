a96e5e00b2354b657098c0a5f08002f2dec207d4
Stephan D Ramalho <sramalho@fordham.edu>
2023-09-01 10:47:34 -0400

    more practice converting if let statements to match

--- pattern_match/match/if_let3.rs
+++ pattern_match/match/if_let3.rs
@@ -9,11 +9,17 @@ fn main() {
   let a = Foo::Qux(10);
 
   // Remove the codes below, using `match` instead 
-  if let Foo::Bar = a {
-      println!("match foo::bar")
-  } else if let Foo::Baz = a {
-      println!("match foo::baz")
-  } else {
-      println!("match others")
+  // if let Foo::Bar = a {
+  //     println!("match foo::bar")
+  // } else if let Foo::Baz = a {
+  //     println!("match foo::baz")
+  // } else {
+  //     println!("match others")
+  // }
+
+  match a {
+    Foo::Bar => println!("match foo::bar"),
+    Foo::Baz => println!("match foo::baz"),
+    _ => println!("match others"),
   }
 }
