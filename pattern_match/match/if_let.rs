a59739d732bcafc9c092a22de7c19b592dd41ee0
Stephan D Ramalho <sramalho@fordham.edu>
2023-09-01 10:40:40 -0400

    practice with if let

--- pattern_match/match/if_let.rs
+++ pattern_match/match/if_let.rs
@@ -1,15 +1,14 @@
 // For some cases, when matching enums, match is too heavy. We can use if let instead.
 
 fn main() {
-  let o = Some(7);
+    let o = Some(7);
 
-  // Remove the whole `match` block, using `if let` instead 
-  match o {
-      Some(i) => {
-          println!("This is a really long string and `{:?}`", i);
+    // Remove the whole `match` block, using `if let` instead 
+    
 
-          println!("Success!");
-      }
-      _ => {}
-  };
+    if let Some(i) = o {
+      println!("This is a really long string and `{:?}`", i);
+
+      println!("Success!");
+    }
 }
