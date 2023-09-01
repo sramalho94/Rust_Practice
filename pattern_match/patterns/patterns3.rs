fc823da04f6c9b4a929a99849cbc5cf274453f94
Stephan D Ramalho <sramalho@fordham.edu>
2023-09-01 11:43:57 -0400

    more practice with @ operator in mattern matching

--- pattern_match/patterns/patterns3.rs
+++ pattern_match/patterns/patterns3.rs
@@ -9,9 +9,9 @@ fn main() {
 
   match msg {
       Message::Hello {
-          id:  3..=7,
+          id: id @ 3..=7,
       } => println!("Found an id in range [3, 7]: {}", id),
-      Message::Hello { id: newid@10 | 11 | 12 } => {
+      Message::Hello { id: newid @ 10 | newid @11 | newid @12 } => {
           println!("Found an id in another range [10, 12]: {}", newid)
       }
       Message::Hello { id } => println!("Found some other id: {}", id),
