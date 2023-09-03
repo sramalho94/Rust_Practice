467381abe3b17b1303df0a307fe6d41e956b6b20
Stephan D Ramalho <sramalho@fordham.edu>
2023-09-03 10:51:33 -0400

    practice using multiple impl blocks in a struct

--- methods/method5.rs
+++ methods/method5.rs
@@ -11,9 +11,11 @@ impl Rectangle {
   fn area(&self) -> u32 {
       self.width * self.height
   }
+}
 
+impl Rectangle {
   fn can_hold(&self, other: &Rectangle) -> bool {
-      self.width > other.width && self.height > other.height
+    self.width > other.width && self.height > other.height
   }
 }
 
