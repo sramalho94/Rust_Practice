7b1e228e3a76893b45e6888718ec5eee205f2a2b
Stephan D Ramalho <sramalho@fordham.edu>
2023-09-03 10:49:53 -0400

    practice with associated functions

--- methods/method4.rs
+++ methods/method4.rs
@@ -9,7 +9,11 @@ impl TrafficLight {
     // 1. Implement an assotiated function `new`,
     // 2. It will return a TrafficLight contains color "red"
     // 3. Must use `Self`, DONT use `TrafficLight` in fn signatures or body
-    pub fn new() 
+    pub fn new() -> Self {
+      Self {
+        color: String::from("red"),
+      }
+    } 
 
     pub fn get_state(&self) -> &str {
         &self.color
