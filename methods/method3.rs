c6e84f6d30969e350a6cbae222826340cbda283a
Stephan D Ramalho <sramalho@fordham.edu>
2023-09-03 10:44:55 -0400

    practice with Method parameters suing Self

--- methods/method3.rs
+++ methods/method3.rs
@@ -6,12 +6,12 @@ struct TrafficLight {
 
 impl TrafficLight {
   // Using `Self` to fill in the blank.
-  pub fn show_state(__)  {
+  pub fn show_state(self: &Self)  {
       println!("the current state is {}", self.color);
   }
 
   // Fill in the blank, DON'T use any variants of `Self`.
-  pub fn change_state(__) {
+  pub fn change_state(&mut self) {
       self.color = "green".to_string()
   }
 }
