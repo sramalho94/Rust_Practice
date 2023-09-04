fc2c27d2db86522a413dfdea32b5ea4a5868de14
Stephan D Ramalho <sramalho@fordham.edu>
2023-09-04 12:27:20 -0400

    created practice files for generics

---
+++ generics_and_traits/generics/generic2.rs
@@ -0,0 +1,11 @@
+// A function call with explicitly specified type parameters looks like: fun::<A, B, ...>().
+// Implement the generic function below.
+fn sum
+
+fn main() {
+    assert_eq!(5, sum(2i8, 3i8));
+    assert_eq!(50, sum(20, 30));
+    assert_eq!(2.46, sum(1.23, 1.23));
+
+    println!("Success!");
+}
