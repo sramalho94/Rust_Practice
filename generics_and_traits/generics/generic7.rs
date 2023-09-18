639e5b9bd717531ce108c727efb16a65c5e263e5
Stephan D Ramalho <sramalho@fordham.edu>
2023-09-18 12:11:29 -0400

    more practice with generics

--- generics_and_traits/generics/generic7.rs
+++ generics_and_traits/generics/generic7.rs
@@ -5,13 +5,13 @@ struct Point<T> {
   y: T,
 }
 
-impl Point<f32> {
-  fn distance_from_origin(&self) -> f32 {
+impl Point<f64> {
+  fn distance_from_origin(&self) -> f64 {
       (self.x.powi(2) + self.y.powi(2)).sqrt()
   }
 }
 
 fn main() {
-  let p = Point{x: 5, y: 10};
+  let p: Point<f64> = Point{x: 5.0, y: 10.0};
   println!("{}",p.distance_from_origin());
 }
