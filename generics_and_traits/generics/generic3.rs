16863564d1ae55fa81366e41e297512ecec7a4ca
Stephan D Ramalho <sramalho@fordham.edu>
2023-09-18 11:48:40 -0400

    practice using generics in structs

--- generics_and_traits/generics/generic3.rs
+++ generics_and_traits/generics/generic3.rs
@@ -1,10 +1,13 @@
 
 // Implement struct Point to make it work.
-
+struct Point<T> {
+  x: T,
+  y: T,
+}
 
 fn main() {
-  let integer = Point { x: 5, y: 10 };
-  let float = Point { x: 1.0, y: 4.0 };
+  let integer: Point<i32> = Point { x: 5, y: 10 };
+  let float: Point<f64> = Point { x: 1.0, y: 4.0 };
 
   println!("Success!");
 }
