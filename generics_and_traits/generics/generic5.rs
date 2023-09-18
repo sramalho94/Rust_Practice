5b17f4c29c4e59c9c093664f56f65beb3c67d350
Stephan D Ramalho <sramalho@fordham.edu>
2023-09-18 11:55:43 -0400

    practice using generics with struct methods

--- generics_and_traits/generics/generic5.rs
+++ generics_and_traits/generics/generic5.rs
@@ -1,18 +1,18 @@
 
 // Add generic for Val to make the code work, DON'T modify the code in `main`.
-struct Val {
-  val: f64,
+struct Val<T> {
+  val: T,
 }
 
-impl Val {
-  fn value(&self) -> &f64 {
+impl<T> Val<T> {
+  fn value(&self) -> &T{
       &self.val
   }
 }
 
 
 fn main() {
-  let x = Val{ val: 3.0 };
-  let y = Val{ val: "hello".to_string()};
+  let x: Val<f64> = Val{ val: 3.0 };
+  let y: Val<String> = Val{ val: "hello".to_string()};
   println!("{}, {}", x.value(), y.value());
 }
