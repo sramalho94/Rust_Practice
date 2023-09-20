a910c5afc442f84338d730b72fb9c44a0fd52a5b
Stephan D Ramalho <sramalho@fordham.edu>
2023-09-20 10:00:16 -0400

    practice with Mul ops trait

--- generics_and_traits/traits/traits3.rs
+++ generics_and_traits/traits/traits3.rs
@@ -5,7 +5,9 @@ use std::ops;
 // Implement fn multiply to make the code work.
 // As mentiond above, `+` needs `T` to implement `std::ops::Add` Trait.
 // So, what about `*`?  You can find the answer here: https://doc.rust-lang.org/core/ops/
-fn multipl
+fn multiply<T: std::ops::Mul<Output = T>>(a: T, b: T)->T{
+    a * b
+}
 
 fn main() {
     assert_eq!(6, multiply(2u8, 3u8));
