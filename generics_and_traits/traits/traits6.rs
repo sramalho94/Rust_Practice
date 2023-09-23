2867aedda67812a68c2bbcffee796aa94edbd6fb
Stephan D Ramalho <sramalho@fordham.edu>
2023-09-23 11:44:11 -0400

    practice using dynamic dispatch

--- generics_and_traits/traits/traits6.rs
+++ generics_and_traits/traits/traits6.rs
@@ -23,16 +23,16 @@ impl Animal for Cow {
 
 // Returns some struct that implements Animal, but we don't know which one at compile time.
 // FIX the errors here, you can make a fake random, or you can use trait object.
-fn random_animal(random_number: f64) -> impl Animal {
+fn random_animal(random_number: f64) -> Box<dyn Animal> {
     if random_number < 0.5 {
-        Sheep {}
+        Box::new(Sheep {})
     } else {
-        Cow {}
+        Box::new(Cow {})
     }
 }
 
 fn main() {
-    let random_number = 0.234;
+    let random_number: f64 = 0.234;
     let animal = random_animal(random_number);
     println!("You've randomly chosen an animal, and it says {}", animal.noise());
 }
