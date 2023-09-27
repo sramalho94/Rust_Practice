02046bda48ee8a7875d8d9d1ed91aa2339a0e177
Stephan D Ramalho <sramalho@fordham.edu>
2023-09-27 12:00:09 -0400

    practice using box dyn

--- generics_and_traits/trait_object/trait_obj1.rs
+++ generics_and_traits/trait_object/trait_obj1.rs
@@ -31,16 +31,16 @@ impl Bird for Swan {
 
 fn main() {
   // FILL in the blank.
-  let duck = __;
+  let duck: Duck = Duck;
   duck.swim();
 
-  let bird = hatch_a_bird(2);
+  let bird: Box<dyn Bird> = hatch_a_bird(2);
   // This bird has forgotten how to swim, so below line will cause an error.
   // bird.swim();
   // But it can quak.
   assert_eq!(bird.quack(), "duck duck");
 
-  let bird = hatch_a_bird(1);
+  let bird: Box<dyn Bird> = hatch_a_bird(1);
   // This bird has forgotten how to fly, so below line will cause an error.
   // bird.fly();
   // But it can quak too.
@@ -50,5 +50,11 @@ fn main() {
 }   
 
 // IMPLEMENT this function.
-fn hatch_a_bird...
+fn hatch_a_bird(species: u8) -> Box<dyn Bird> {
+  match species {
+    1 => Box::new(Swan),
+    2 => Box::new(Duck),
+    _ => panic!(),
+  }
+}
 
