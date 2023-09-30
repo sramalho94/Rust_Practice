ee6c4235af495efa690769aa9b20cf8d980b9518
Stephan D Ramalho <sramalho@fordham.edu>
2023-09-30 10:49:47 -0400

    practice implementing statis dispatch

--- generics_and_traits/trait_object/trait_obj5.rs
+++ generics_and_traits/trait_object/trait_obj5.rs
@@ -6,20 +6,20 @@ trait MyTrait {
 }
 
 impl MyTrait for u32 {
-  fn f(&self) -> Self { 42 }
+  fn f(&self) -> u32 { 42 }
 }
 
 impl MyTrait for String {
   fn f(&self) -> Self { self.clone() }
 }
 
-fn my_function(x: Box<dyn MyTrait>)  {
-  x.f()
+fn my_function<T: MyTrait>(x: T)  {
+  x.f();
 }
 
 fn main() {
-  my_function(Box::new(13_u32));
-  my_function(Box::new(String::from("abc")));
+  my_function(13_u32);
+  my_function(String::from("abc"));
 
   println!("Success!");
 }
