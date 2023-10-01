55c9d70cfdcd598b98a163d50da1125c00f78069
Stephan D Ramalho <sramalho@fordham.edu>
2023-10-01 10:59:37 -0400

    dynamic dispatch solution

--- generics_and_traits/trait_object/trait_obj5.rs
+++ generics_and_traits/trait_object/trait_obj5.rs
@@ -2,24 +2,24 @@
 // Use at least two approaches to make it work.
 // DON'T add/remove any code line.
 trait MyTrait {
-  fn f(&self) -> Self;
+  fn f(&self) -> Box<dyn MyTrait>;
 }
 
 impl MyTrait for u32 {
-  fn f(&self) -> u32 { 42 }
+  fn f(&self) -> Box<dyn MyTrait> { Box::new(42) }
 }
 
 impl MyTrait for String {
-  fn f(&self) -> Self { self.clone() }
+  fn f(&self) -> Box<dyn MyTrait> { Box::new(self.clone()) }
 }
 
-fn my_function<T: MyTrait>(x: T) -> T  {
+fn my_function(x: Box<dyn MyTrait>) -> Box<dyn MyTrait>{
   x.f()
 }
 
 fn main() {
-  my_function(13_u32);
-  my_function(String::from("abc"));
+  my_function(Box::new(13_u32));
+  my_function(Box::new(String::from("abc")));
 
   println!("Success!");
 }
