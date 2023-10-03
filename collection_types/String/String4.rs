52b2445ca7cecf1819ff5f53a726108df8cc21c7
Stephan D Ramalho <sramalho@fordham.edu>
2023-10-03 11:22:54 -0400

    practice with enumerating through strings

--- collection_types/String/String4.rs
+++ collection_types/String/String4.rs
@@ -1,15 +1,15 @@
 
 // FILL in the blank and FIX errors
 fn main() {
-  let s = String::from("hello, 世界");
-  let slice1 = s[0]; //tips: `h` only takes 1 byte in UTF8 format
+  let s : String = String::from("hello, 世界");
+  let slice1: &str = &s[..1]; //tips: `h` only takes 1 byte in UTF8 format
   assert_eq!(slice1, "h");
 
-  let slice2 = &s[3..5]; // Tips: `中`  takes 3 bytes in UTF8 format
+  let slice2: &str = &s[7..10]; // Tips: `中`  takes 3 bytes in UTF8 format
   assert_eq!(slice2, "世");
   
   // Iterate through all chars in s
-  for (i, c) in s.__ {
+  for (i, c) in s.chars().enumerate(){
       if i == 7 {
           assert_eq!(c, '世')
       }
