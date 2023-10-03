c1b76b962ed6b4e2e253cbf51ab2e59630f26767
Stephan D Ramalho <sramalho@fordham.edu>
2023-10-03 11:19:14 -0400

    practice with understanding heap allocation with Strings

--- collection_types/String/String3.rs
+++ collection_types/String/String3.rs
@@ -1,18 +1,18 @@
 
 // Question: how many heap allocations are happening here?
-// Your answer: 
+// Your answer: 2
 fn main() {  
   // Create a String type based on `&str`
   // The type of string literals is `&str`
- let s: String = String::from("hello, world!");
+  let s: String = String::from("hello, world!"); // 1
 
  // Create a slice point to String `s`
- let slice: &str = &s;
+  let slice: &str = &s;
 
  // Create a String type based on the recently created slice
- let s: String = slice.to_string();
+  let s: String = slice.to_string(); // 2
 
- assert_eq!(s, "hello, world!");
+  assert_eq!(s, "hello, world!");
 
- println!("Success!");
+  println!("Success!");
 }
