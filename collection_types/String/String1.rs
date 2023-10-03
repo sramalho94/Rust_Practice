7abb686f06e5b3d50157a0b695d54800dd2a9373
Stephan D Ramalho <sramalho@fordham.edu>
2023-10-03 11:13:27 -0400

    practice with String, push_str and push methods

--- collection_types/String/String1.rs
+++ collection_types/String/String1.rs
@@ -3,11 +3,11 @@
 // 1. Don't use `to_string()`
 // 2. Don't add/remove any code line
 fn main() {
-  let mut s: String = "hello, ";
-  s.push_str("world".to_string());
-  s.push(__);
+  let mut s: String = String::from("hello, ");
+  s.push_str("world");
+  s.push('!');
 
-  move_ownership(s);
+  move_ownership(s.clone());
 
   assert_eq!(s, "hello, world!");
 
