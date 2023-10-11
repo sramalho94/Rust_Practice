281f3a9664f0b00445dfb3fba0ce97d6bd20276f
Stephan D Ramalho <sramalho@fordham.edu>
2023-10-11 14:48:50 -0400

    Practice converting arrays into vectors

--- collection_types/vector/vector1.rs
+++ collection_types/vector/vector1.rs
@@ -2,20 +2,24 @@
 fn main() {
   let arr: [u8; 3] = [1, 2, 3];
   
-  let v = Vec::from(arr);
+  let v: Vec<u8> = Vec::from(arr);
   is_vec(v);
 
   let v = vec![1, 2, 3];
-  is_vec(v);
+  is_vec(v.clone());
 
   // vec!(..) and vec![..] are same macros, so
   let v = vec!(1, 2, 3);
-  is_vec(v);
+  is_vec(v.clone());
   
   // In code below, v is Vec<[u8; 3]> , not Vec<u8>
   // USE Vec::new and `for` to rewrite the below code 
-  let v1 = vec!(arr);
-  is_vec(v1);
+  let mut v1 = Vec::new();
+  is_vec(v1.clone());
+
+  for i in &v {
+    v1.push(*i);
+  }
 
   assert_eq!(v, v1);
 
