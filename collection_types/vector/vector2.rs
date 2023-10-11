9155cce0ba667d3b47ad2a8a8b12c358b18ad85f
Stephan D Ramalho <sramalho@fordham.edu>
2023-10-11 14:51:58 -0400

    Practice using extend method for vectors

--- collection_types/vector/vector2.rs
+++ collection_types/vector/vector2.rs
@@ -2,12 +2,12 @@
 
 // FILL in the blank
 fn main() {
-  let mut v1 = Vec::from([1, 2, 4]);
+  let mut v1: Vec<i32> = Vec::from([1, 2, 4]);
   v1.pop();
   v1.push(3);
   
-  let mut v2 = Vec::new();
-  v2.__;
+  let mut v2: Vec<i32> = Vec::new();
+  v2.extend(&v1);
 
   assert_eq!(v1, v2);
 
