6711e36c27b8d2d53578fcc6b726b2c319628e26
Stephan D Ramalho <sramalho@fordham.edu>
2023-11-07 11:32:16 -0500

    Add more practice with vectors

--- collection_types/vector/vector3.rs
+++ collection_types/vector/vector3.rs
@@ -3,25 +3,25 @@
 fn main() {
   // Array -> Vec
   // impl From<[T; N]> for Vec
-  let arr = [1, 2, 3];
-  let v1 = __(arr);
-  let v2: Vec<i32> = arr.__();
+  let arr: [i32; 3] = [1, 2, 3];
+  let v1: Vec<i32> = Vec::from(arr);
+  let v2: Vec<i32> = arr.into();
 
   assert_eq!(v1, v2);
 
   
   // String -> Vec
   // impl From<String> for Vec
-  let s = "hello".to_string();
-  let v1: Vec<u8> = s.__();
+  let s: String = "hello".to_string();
+  let v1: Vec<u8> = s.into();
 
   let s = "hello".to_string();
-  let v2 = s.into_bytes();
+  let v2: Vec<u8> = s.into_bytes();
   assert_eq!(v1, v2);
 
   // impl<'_> From<&'_ str> for Vec
-  let s = "hello";
-  let v3 = Vec::__(s);
+  let s: &str = "hello";
+  let v3: Vec<u8> = Vec::from(s);
   assert_eq!(v2, v3);
 
   // Iterators can be collected into vectors
