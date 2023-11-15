c24966576a5c60e347383fa9efce81690d4d55a0
Stephan D Ramalho <sramalho@fordham.edu>
2023-11-15 12:42:53 -0500

    More practice with vectors

--- collection_types/vector/vector4.rs
+++ collection_types/vector/vector4.rs
@@ -2,12 +2,17 @@
 // FIX the error and IMPLEMENT the code
 fn main() {
   let mut v = Vec::from([1, 2, 3]);
+
   for i in 0..5 {
-      println!("{:?}", v[i])
+      println!("{:?}", v.get(i)) // Option<i32>
   }
 
   for i in 0..5 {
      // IMPLEMENT the code here...
+      match v.get(i){
+        Some(e) => v[i] = e + 1,
+        None => v.push(i + 2)
+      }
   }
   
   assert_eq!(v, vec![2, 3, 4, 5, 6]);
