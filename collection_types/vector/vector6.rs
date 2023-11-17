8c1f93a34013d25be446184f24524bc8f9dfb932
Stephan D Ramalho <sramalho@fordham.edu>
2023-11-17 12:55:16 -0500

    Practice with vector length and capacity

--- collection_types/vector/vector6.rs
+++ collection_types/vector/vector6.rs
@@ -7,15 +7,15 @@ fn main() {
   let mut vec = Vec::with_capacity(10);
 
   // The vector contains no items, even though it has capacity for more
-  assert_eq!(vec.len(), __);
+  assert_eq!(vec.len(), 0);
   assert_eq!(vec.capacity(), 10);
 
   // These are all done without reallocating...
   for i in 0..10 {
       vec.push(i);
   }
-  assert_eq!(vec.len(), __);
-  assert_eq!(vec.capacity(), __);
+  assert_eq!(vec.len(), 10);
+  assert_eq!(vec.capacity(), 10);
 
   // ...but this may make the vector reallocate
   vec.push(11);
@@ -24,13 +24,13 @@ fn main() {
 
 
   // Fill in an appropriate value to make the `for` done without reallocating 
-  let mut vec = Vec::with_capacity(__);
+  let mut vec = Vec::with_capacity(100);
   for i in 0..100 {
       vec.push(i);
   }
 
-  assert_eq!(vec.len(), __);
-  assert_eq!(vec.capacity(), __);
+  assert_eq!(vec.len(), 100);
+  assert_eq!(vec.capacity(), 100);
   
   println!("Success!");
 }
