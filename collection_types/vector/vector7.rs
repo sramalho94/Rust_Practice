d64374d8dafa1b7b0b76118552090b8c36f2fcac
Stephan D Ramalho <sramalho@fordham.edu>
2023-11-18 15:11:06 -0500

    Practice storing data in enum vectors

--- collection_types/vector/vector7.rs
+++ collection_types/vector/vector7.rs
@@ -1,11 +1,14 @@
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 enum IpAddr {
     V4(String),
     V6(String),
 }
 fn main() {
     // FILL in the blank
-    let v : Vec<IpAddr>= __;
+    let v: Vec<IpAddr>= vec![
+        IpAddr::V4("127.0.0.1".to_string()),
+        IpAddr::V6("::1".to_string())
+    ];
     
     // Comparing two enums need to derive the PartialEq trait
     assert_eq!(v[0], IpAddr::V4("127.0.0.1".to_string()));
