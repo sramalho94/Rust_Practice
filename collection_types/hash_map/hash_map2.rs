7872cb2ebf0c2d377a97008150f3c72805233772
Stephan D Ramalho <sramalho@fordham.edu>
2023-11-28 13:44:19 -0500

    Practice converting arrays to hash maps

--- collection_types/hash_map/hash_map2.rs
+++ collection_types/hash_map/hash_map2.rs
@@ -1,20 +1,20 @@
 
 use std::collections::HashMap;
 fn main() {
-    let teams = [
+    let teams: [(&str, i32);3] = [
         ("Chinese Team", 100),
         ("American Team", 10),
         ("France Team", 50),
     ];
 
-    let mut teams_map1 = HashMap::new();
+    let mut teams_map1: HashMap<&str, i32> = HashMap::new();
     for team in &teams {
         teams_map1.insert(team.0, team.1);
     }
 
     // IMPLEMENT team_map2 in two ways
     // Tips: one of the approaches is to use `collect` method
-    let teams_map2...
+    let teams_map2: HashMap<&str, i32> = HashMap::from(teams);
 
     assert_eq!(teams_map1, teams_map2);
 
